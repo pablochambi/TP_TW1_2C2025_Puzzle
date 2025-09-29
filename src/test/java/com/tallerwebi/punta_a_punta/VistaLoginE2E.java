72ff877060665f5733c23ac89a0b7cf5fe68c33c
pablochambi <pablochambi577@hotmail.com>
2025-09-29 11:24:31 -0300

    comente archivo VistaLoginE2E porque todavia no estamos usando la bases de datos.

--- src/test/java/com/tallerwebi/punta_a_punta/VistaLoginE2E.java
+++ src/test/java/com/tallerwebi/punta_a_punta/VistaLoginE2E.java
@@ -48,73 +48,75 @@ public class VistaLoginE2E {
         context.close();
     }
 
-    @Test
-    void deberiaDecirUNLAMEnElNavbar() throws MalformedURLException {
-        dadoQueElUsuarioEstaEnLaVistaDeLogin();
-        entoncesDeberiaVerUNLAMEnElNavbar();
-    }
+//    @Test
+//    void deberiaDecirUNLAMEnElNavbar() throws MalformedURLException {
+//        dadoQueElUsuarioEstaEnLaVistaDeLogin();
+//        entoncesDeberiaVerUNLAMEnElNavbar();
+//    }
+//
+//    @Test
+//    void deberiaDarUnErrorAlIntentarIniciarSesionConUnUsuarioQueNoExiste() {
+//        dadoQueElUsuarioCargaSusDatosDeLoginCon("damian@unlam.edu.ar", "unlam");
+//        cuandoElUsuarioTocaElBotonDeLogin();
+//        entoncesDeberiaVerUnMensajeDeError();
+//    }
+//
+//    @Test
+//    void deberiaNavegarAlHomeSiElUsuarioExiste() throws MalformedURLException {
+//        dadoQueElUsuarioCargaSusDatosDeLoginCon("test@unlam.edu.ar", "test");
+//        cuandoElUsuarioTocaElBotonDeLogin();
+//        entoncesDeberiaSerRedirigidoALaVistaDeHome();
+//    }
+//
+//    @Test
+//    void deberiaRegistrarUnUsuarioEIniciarSesionExistosamente() throws MalformedURLException {
+//        dadoQueElUsuarioNavegaALaVistaDeRegistro();
+//        dadoQueElUsuarioSeRegistraCon("juan@unlam.edu.ar", "123456");
+//        dadoQueElUsuarioEstaEnLaVistaDeLogin();
+//        dadoQueElUsuarioCargaSusDatosDeLoginCon("juan@unlam.edu.ar", "123456");
+//        cuandoElUsuarioTocaElBotonDeLogin();
+//        entoncesDeberiaSerRedirigidoALaVistaDeHome();
+//    }
+//
+//    private void entoncesDeberiaVerUNLAMEnElNavbar() {
+//        String texto = vistaLogin.obtenerTextoDeLaBarraDeNavegacion();
+//        assertThat("UNLAM", equalToIgnoringCase(texto));
+//    }
+//
+//    private void dadoQueElUsuarioEstaEnLaVistaDeLogin() throws MalformedURLException {
+//        URL urlLogin = vistaLogin.obtenerURLActual();
+//        assertThat(urlLogin.getPath(), matchesPattern("^/spring/login(?:;jsessionid=[^/\\s]+)?$"));
+//    }
+//
+//    private void cuandoElUsuarioTocaElBotonDeLogin() {
+//        vistaLogin.darClickEnIniciarSesion();
+//    }
+//
+//    private void entoncesDeberiaSerRedirigidoALaVistaDeHome() throws MalformedURLException {
+//        URL url = vistaLogin.obtenerURLActual();
+//        assertThat(url.getPath(), matchesPattern("^/spring/home(?:;jsessionid=[^/\\s]+)?$"));
+//    }
+//
+//    private void entoncesDeberiaVerUnMensajeDeError() {
+//        String texto = vistaLogin.obtenerMensajeDeError();
+//        assertThat("Error Usuario o clave incorrecta", equalToIgnoringCase(texto));
+//    }
+//
+//    private void dadoQueElUsuarioCargaSusDatosDeLoginCon(String email, String clave) {
+//        vistaLogin.escribirEMAIL(email);
+//        vistaLogin.escribirClave(clave);
+//    }
+//
+//    private void dadoQueElUsuarioNavegaALaVistaDeRegistro() {
+//        vistaLogin.darClickEnRegistrarse();
+//    }
+//
+//    private void dadoQueElUsuarioSeRegistraCon(String email, String clave) {
+//        VistaNuevoUsuario vistaNuevoUsuario = new VistaNuevoUsuario(context.pages().get(0));
+//        vistaNuevoUsuario.escribirEMAIL(email);
+//        vistaNuevoUsuario.escribirClave(clave);
+//        vistaNuevoUsuario.darClickEnRegistrarme();
+//    }
 
-    @Test
-    void deberiaDarUnErrorAlIntentarIniciarSesionConUnUsuarioQueNoExiste() {
-        dadoQueElUsuarioCargaSusDatosDeLoginCon("damian@unlam.edu.ar", "unlam");
-        cuandoElUsuarioTocaElBotonDeLogin();
-        entoncesDeberiaVerUnMensajeDeError();
-    }
 
-    @Test
-    void deberiaNavegarAlHomeSiElUsuarioExiste() throws MalformedURLException {
-        dadoQueElUsuarioCargaSusDatosDeLoginCon("test@unlam.edu.ar", "test");
-        cuandoElUsuarioTocaElBotonDeLogin();
-        entoncesDeberiaSerRedirigidoALaVistaDeHome();
-    }
-
-    @Test
-    void deberiaRegistrarUnUsuarioEIniciarSesionExistosamente() throws MalformedURLException {
-        dadoQueElUsuarioNavegaALaVistaDeRegistro();
-        dadoQueElUsuarioSeRegistraCon("juan@unlam.edu.ar", "123456");
-        dadoQueElUsuarioEstaEnLaVistaDeLogin();
-        dadoQueElUsuarioCargaSusDatosDeLoginCon("juan@unlam.edu.ar", "123456");
-        cuandoElUsuarioTocaElBotonDeLogin();    
-        entoncesDeberiaSerRedirigidoALaVistaDeHome();
-    }
-
-    private void entoncesDeberiaVerUNLAMEnElNavbar() {
-        String texto = vistaLogin.obtenerTextoDeLaBarraDeNavegacion();
-        assertThat("UNLAM", equalToIgnoringCase(texto));
-    }
-
-    private void dadoQueElUsuarioEstaEnLaVistaDeLogin() throws MalformedURLException {
-        URL urlLogin = vistaLogin.obtenerURLActual();
-        assertThat(urlLogin.getPath(), matchesPattern("^/spring/login(?:;jsessionid=[^/\\s]+)?$"));
-    }
-
-    private void cuandoElUsuarioTocaElBotonDeLogin() {
-        vistaLogin.darClickEnIniciarSesion();
-    }
-
-    private void entoncesDeberiaSerRedirigidoALaVistaDeHome() throws MalformedURLException {
-        URL url = vistaLogin.obtenerURLActual();
-        assertThat(url.getPath(), matchesPattern("^/spring/home(?:;jsessionid=[^/\\s]+)?$"));
-    }
-
-    private void entoncesDeberiaVerUnMensajeDeError() {
-        String texto = vistaLogin.obtenerMensajeDeError();
-        assertThat("Error Usuario o clave incorrecta", equalToIgnoringCase(texto));
-    }
-
-    private void dadoQueElUsuarioCargaSusDatosDeLoginCon(String email, String clave) {
-        vistaLogin.escribirEMAIL(email);
-        vistaLogin.escribirClave(clave);
-    }
-
-    private void dadoQueElUsuarioNavegaALaVistaDeRegistro() {
-        vistaLogin.darClickEnRegistrarse();
-    }
-
-    private void dadoQueElUsuarioSeRegistraCon(String email, String clave) {
-        VistaNuevoUsuario vistaNuevoUsuario = new VistaNuevoUsuario(context.pages().get(0));
-        vistaNuevoUsuario.escribirEMAIL(email);
-        vistaNuevoUsuario.escribirClave(clave);
-        vistaNuevoUsuario.darClickEnRegistrarme();
-    }
 }
